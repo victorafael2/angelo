b7aebac4d4ad63aba01f9390d2a2b35ffaf8cacb
Victor Rafael de Oliveira Silva <106404461+victorafael2@users.noreply.github.com>
2023-08-28 11:13:14 -0300

    logo

--- index.php
+++ index.php
@@ -54,12 +54,12 @@ exit();
     <!-- ===============================================-->
     <!--    Favicons-->
     <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/png/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/png/logo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/png/logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/png/logo.png">
     <link rel="manifest" href="assets/img/favicons/manifest.json">
-    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+    <meta name="msapplication-TileImage" content="assets/png/logo.png">
     <meta name="theme-color" content="#ffffff">
     <script src="vendors/imagesloaded/imagesloaded.pkgd.min.js"></script>
     <script src="vendors/simplebar/simplebar.min.js"></script>
